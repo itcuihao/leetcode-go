a59cbb415009abb18eacbe46d00293861cc747f4
haoc7 <it_cuihao@163.com>
2018-01-30 23:44:14 +0800

    reverse integer

--- 0007.reverse-integer/run.go
+++ 0007.reverse-integer/run.go
@@ -10,11 +10,9 @@ func reverse(x int) int {
 	s := strconv.Itoa(x)
 	i := make([]rune, len(s))
 	l := len(s)
-	j := 1
 
 	if x < 0 {
 		s = s[1:]
-		j = -1
 	}
 
 	for k, v := range s {
@@ -31,15 +29,15 @@ func reverse(x int) int {
 
 	s = string(i)
 
-	x, err := strconv.Atoi(s)
+	j, err := strconv.Atoi(s)
 	if err != nil {
 		return 0
 	}
-	if j < 0 {
-		x = ^x + 1 // 按位取反 +1 得到对应负数
+	if x < 0 {
+		j = ^j + 1 // 按位取反 +1 得到对应负数
 	}
-	if x > math.MaxInt32 || x < math.MinInt32 {
+	if j > math.MaxInt32 || j < math.MinInt32 {
 		return 0
 	}
-	return x
+	return j
 }
