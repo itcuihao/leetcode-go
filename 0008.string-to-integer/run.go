0218a52a0763842d9b372e2196dc4969087c86f7
HaoC_it <it_cuihao@163.com>
2018-02-12 15:33:58 +0800

    atoi

--- 0008.string-to-integer/run.go
+++ 0008.string-to-integer/run.go
@@ -1,20 +1,60 @@
 package integer
 
 import (
-	"strconv"
+	"math"
+	"strings"
 )
 
 func myAtoi(str string) int {
-	var b bool
+	str = strings.TrimSpace(str)
+
+	ls := len(str)
+	if ls == 0 {
+		return 0
+	}
+
+	myi, m, plus, less := 0, 1, false, false
 	if str[0] == '-' {
 		str = str[1:]
-		b = true
+		less = true
+	} else if str[0] == '+' {
+		str = str[1:]
+		plus = true
 	}
-	for _, v := range str {
-		if v < 48 || v > 57 {
-			return 0
-		}
 
+	if plus || less {
+		ls--
 	}
-	strconv.Atoi()
+
+	for k, v := range str {
+		if v < 48 || v > 57 {
+			str = str[:k]
+			ls = k
+			break
+		}
+	}
+	for i := ls - 1; i >= 0; i-- {
+		myi += int(str[i]-48) * m
+		if isOver(myi) {
+			if less {
+				return math.MinInt32
+			}
+			return math.MaxInt32
+		}
+		m *= 10
+	}
+
+	if less {
+		myi = ^myi + 1
+	}
+	return myi
+}
+
+func isOver(i int) bool {
+	if i > math.MaxInt32 {
+		return true
+	} else if i < math.MinInt32 {
+		return true
+	}
+	return false
 }
--- 0008.string-to-integer/run_test.go
+++ 0008.string-to-integer/run_test.go
@@ -3,6 +3,9 @@ package integer
 import "testing"
 
 func TestMyAtoi(t *testing.T) {
-	s := "123"
+	// s := "-123a..."
+	// s := "    010"
+	// s := "-2147483648"
+	s := "2147483648"
 	t.Log(myAtoi(s))
 }
