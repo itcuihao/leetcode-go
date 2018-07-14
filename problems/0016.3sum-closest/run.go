3ffc5a6ec66821250349544782c3507868544b94
itcuihao <it_cuihao@163.com>
2018-07-14 23:29:47 +0800

    3sum cloest

--- problems/0016.3sum-closest/run.go
+++ problems/0016.3sum-closest/run.go
@@ -1,6 +1,33 @@
 package closest
 
-func threeSumClosest(nums []int, target int) int {
+import (
+	"archive/tar"
+	"sort"
+)
 
-	return 0
-}
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	l,r:=0,len(nums,target)
+
+	for l<r{
+		s:=nums[i]+nums[j]+nums[k]
+
+		switch true {
+		case s<target:
+			i++
+			if data<target-s{
+				data=target -s{
+					data=target-s
+					row=s
+				}
+			}
+		case s > target:
+			r--
+			if delta > s-target {
+				delta = s - target
+				res = s
+			}
+		default:
+			return s
+		}
+	}
