f3846749294634710a8307cf191ca85ca083f79d
itcuihao <it_cuihao@163.com>
2018-07-25 23:31:45 +0800

    leter combinations of a phone number

--- problems/0017.letter-combinations-of-a-phone-number/run.go
+++ problems/0017.letter-combinations-of-a-phone-number/run.go
@@ -1,6 +1,26 @@
 package closest
 
-func letterCombinations(digits string) []string {
 
-	return nil
+var jp = []string{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+func letterCombinations(digits string) []string {
+	ld := len(digits)
+	// 初始化
+	r := []string{}
+	if ld == 0 {
+		return r
+	}
+r=[]string{""}
+	for i := 0; i < ld; i++ {
+		var tmp []string
+		// 获取jp对应的下标
+		index := digits[i] - '0'
+		for _, k := range r {
+			for _, j := range jp[index] {
+				tmp = append(tmp, k+string(j))
+			}
+		}
+		// 重新给r赋值
+		r = tmp
+	}
+	return r
 }
