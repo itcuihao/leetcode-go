5946fcdd601e7af184f55b8196ea476a426679f9
itcuihao <it_cuihao@163.com>
2018-07-30 00:10:19 +0800

    4sum

--- problems/0018.4sum/run.go
+++ problems/0018.4sum/run.go
@@ -12,5 +12,40 @@ func fourSum(nums []int, target int) [][]int {
 	}
 
 	sort.Ints(nums)
-	return nil
+
+	for i := 0; i < ln-3; i++ {
+		if i > 0 && nums[i-1] == nums[i] {
+			continue
+		}
+		for j := i + 1; j < ln-2; j++ {
+			if j > i+1 && nums[j-1] == nums[j] {
+				continue
+			}
+			s, e := j+1, ln-1
+			for s < e {
+				sum := nums[i] + nums[j] + nums[s] + nums[e]
+
+				if sum == target {
+					d := []int{nums[i], nums[j], nums[s], nums[e]}
+
+					data = append(data, d)
+					for s < e && nums[s] == nums[s+1] {
+						s++
+					}
+					for s < e && nums[e-1] == nums[e] {
+						e--
+					}
+					s++
+					e--
+				} else if sum < target {
+					s++
+				} else {
+					e--
+				}
+			}
+		}
+
+	}
+
+	return data
 }
--- problems/0018.4sum/run_test.go
+++ problems/0018.4sum/run_test.go
@@ -9,8 +9,8 @@ func TestRun(t *testing.T) {
 
 	target := 0
 
-	nums := []int{1, 0, -1, 0, -2, 2}
-	// s := letterCombinations(target)
+	// nums := []int{1, 0, -1, 0, -2, 2}
+	nums := []int{0, 0, 0, 0}
 	s := fourSum(nums, target)
 	fmt.Println(s)
 }
