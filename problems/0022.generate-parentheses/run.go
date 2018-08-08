8b8d2139701bc789854e8d9d957fa228a17f7f87
itcuihao <it_cuihao@163.com>
2018-08-09 00:20:12 +0800

    22

--- problems/0022.generate-parentheses/run.go
+++ problems/0022.generate-parentheses/run.go
@@ -1,5 +1,25 @@
 package parentheses
 
+import "fmt"
+
 func generateParenthesis(n int) []string {
 
+	data := []string{}
+	dfs("", n, n, &data)
+	fmt.Println(data)
+	return data
+}
+
+func dfs(cur string, l, r int, data *[]string) {
+
+	if l == 0 && r == 0 {
+		*data = append(*data, cur)
+		return
+	}
+	if l > 0 {
+		dfs(cur+"(", l-1, r, data)
+	}
+	if r > 0 && l < r {
+		dfs(cur+")", l, r-1, data)
+	}
 }
