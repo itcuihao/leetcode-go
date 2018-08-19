80ca66fe2726375fda514c8afbb4bc10ce2cf0a9
itcuihao <it_cuihao@163.com>
2018-08-19 23:43:48 +0800

    25

--- problems/0025.reverse-nodes-in-k-group/run.go
+++ problems/0025.reverse-nodes-in-k-group/run.go
@@ -13,5 +13,23 @@ import "leetcode-go/structure"
 // 时间复杂度：0(n)
 // 空间复杂度：0(1)
 func reverseKGroup(head *structure.ListNode, k int) *structure.ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	count := 0
+	cur := head
+
+	for cur != nil && count != k {
+		cur = cur.Next
+		count++
+	}
+
+	if count == k {
+		cur = reversK(cur, k)
+		for count > 0 {
+
+		}
+	}
 	return nil
 }
