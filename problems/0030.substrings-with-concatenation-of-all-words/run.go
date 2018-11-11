739a909c8a162f8693031904afc74b3ad3a9adca
itcuihao <it_cuihao@163.com>
2018-11-11 23:02:20 +0800

    add 30

--- problems/0030.substrings-with-concatenation-of-all-words/run.go
+++ problems/0030.substrings-with-concatenation-of-all-words/run.go
@@ -1,6 +1,59 @@
 package words
 
-func findSubstring(s string, words []string) []int {
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
-	return nil
+func findSubstring(s string, words []string) []int {
+	if len(words) == 0 {
+		return nil
+	}
+	var (
+		a      array
+		length []int
+	)
+	a.permutation(words, 0, len(words))
+	max := len(a.eles[0])
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s)
+
+		for _, v := range a.eles {
+			l := strings.Index(s, v)
+			if l > -1 {
+				length = append(length, l)
+			}
+		}
+		if len(s) == max {
+			break
+		}
+		s = s[max:]
+		fmt.Println(length)
+	}
+	sort.Ints(length)
+	for i := len(length) - 1; i > 0; i-- {
+		if length[i] == length[i-1] {
+			length = append(length[:i-1], length[i:]...)
+		}
+	}
+	return length
+}
+
+type array struct {
+	eles []string
+}
+
+func (a *array) permutation(words []string, l, h int) {
+
+	if l == h-1 {
+		a.eles = append(a.eles, strings.Join(words, ""))
+	} else {
+		for i := l; i < h; i++ {
+			words[i], words[l] = words[l], words[i]
+			a.permutation(words, l+1, h)
+			words[i], words[l] = words[l], words[i]
+
+		}
+	}
 }
--- problems/0030.substrings-with-concatenation-of-all-words/run_test.go
+++ problems/0030.substrings-with-concatenation-of-all-words/run_test.go
@@ -1,12 +1,26 @@
 package words
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
-	s := "barfoothefoobarman"
+
+	// s := "wordgoodgoodgoodbestword"
+	// words := []string{"word", "good", "best", "good"}
+	// s := "barfoothefoobarman"
+	// words := []string{"foo", "bar"}
+	s := "foobarfoobar"
 	words := []string{"foo", "bar"}
+
 	i := findSubstring(s, words)
-	t.Log(i)
+	fmt.Println(i)
+}
+
+func TestArray(t *testing.T) {
+	var a array
+	words := []string{"a", "b", "c"}
+	a.permutation(words, 0, len(words))
+	fmt.Println(a.eles)
 }
