29bf06d1f53f715b0ad2d2452ab738e8b3a7254c
itcuihao <it_cuihao@163.com>
2018-12-02 23:44:22 +0800

    30

--- problems/0030.substrings-with-concatenation-of-all-words/run_test.go
+++ problems/0030.substrings-with-concatenation-of-all-words/run_test.go
@@ -9,10 +9,12 @@ func TestRun(t *testing.T) {
 
 	// s := "wordgoodgoodgoodbestword"
 	// words := []string{"word", "good", "best", "good"}
-	// s := "barfoothefoobarman"
-	// words := []string{"foo", "bar"}
-	s := "foobarfoobar"
+	s := "barfoothefoobarman"
 	words := []string{"foo", "bar"}
+	// 	"wordgoodgoodgoodbestword"
+	// ["word","good","best","word"]
+	// s := "wordgoodgoodgoodbestword"
+	// words := []string{"word", "good", "best", "word"}
 
 	i := findSubstring(s, words)
 	fmt.Println(i)
