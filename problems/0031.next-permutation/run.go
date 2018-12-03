c78d95a7c319ee5d0a205ee9d873fb8ef393bd33
itcuihao <it_cuihao@163.com>
2018-12-03 23:15:14 +0800

    add 31

---
+++ problems/0031.next-permutation/run.go
@@ -0,0 +1,5 @@
+package permutation
+
+func nextPermutation(nums []int) {
+
+}
---
+++ problems/0031.next-permutation/run_test.go
@@ -0,0 +1,13 @@
+package permutation
+
+import (
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+
+	words := []int{1, 2, 3}
+
+	nextPermutation(words)
+
+}
