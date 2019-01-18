b393610003cd9baa91ff33437018e34015a2e3e7
itcuihao <it_cuihao@163.com>
2019-01-18 23:56:59 +0800

    31

--- problems/0031.next-permutation/run.go
+++ problems/0031.next-permutation/run.go
@@ -1,5 +1,44 @@
 package permutation
 
-func nextPermutation(nums []int) {
+import "fmt"
 
+func nextPermutation(nums []int) {
+	if nums == nil || len(nums) == 0 {
+		return
+	}
+
+	ln := len(nums)
+	firstSmall := -1
+	for i := ln - 2; i >= 0; i-- {
+		if nums[i] < nums[i+1] {
+			firstSmall = i
+			break
+		}
+	}
+
+	fmt.Println("firstSmall:", firstSmall)
+	if firstSmall == -1 {
+		reverse(nums, 0, ln-1)
+		return
+	}
+
+	firstLarge := -1
+	for i := ln - 1; i > firstSmall; i-- {
+		if nums[i] > nums[firstSmall] {
+			firstLarge = i
+			break
+		}
+	}
+	fmt.Println("firstLarge:", firstLarge)
+
+	nums[firstSmall+1], nums[firstLarge-1] = nums[firstLarge], nums[firstSmall]
+	reverse(nums, firstSmall+1, ln-1)
+	// reverse(nums, firstSmall, firstLarge)
+	return
+}
+
+func reverse(nums []int, l, r int) {
+	for ; l < r; l, r = l+1, r-1 {
+		nums[l], nums[r] = nums[r], nums[l]
+	}
 }
--- problems/0031.next-permutation/run_test.go
+++ problems/0031.next-permutation/run_test.go
@@ -1,13 +1,31 @@
 package permutation
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
 
+	words := [][]int{
+		{1, 3, 2},
+		// {1, 2, 3},
+		// {3, 2, 1},
+		// {1, 1, 5},
+	}
+
+	for _, word := range words {
+		fmt.Println("old:", word)
+		nextPermutation(word)
+		fmt.Println("new:", word)
+	}
+
+}
+func TestReverse(t *testing.T) {
+
 	words := []int{1, 2, 3}
 
-	nextPermutation(words)
+	reverse(words, 0, 1)
 
+	fmt.Println(words)
 }
