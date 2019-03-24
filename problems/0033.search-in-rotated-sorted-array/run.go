23fc25f345ec73a5a75b60635721eb0d7e83ef65
itcuihao <it_cuihao@163.com>
2019-03-24 15:54:43 +0800

    33

--- problems/0033.search-in-rotated-sorted-array/run.go
+++ problems/0033.search-in-rotated-sorted-array/run.go
@@ -1,6 +1,28 @@
 package array
 
 func search(nums []int, target int) int {
+	ln := len(nums)
+	if ln == 0 {
+		return -1
+	}
 
-	return 0
+	n := -1
+
+	for i := 0; i < ln; i++ {
+		if nums[i] > target {
+			break
+		} else if nums[i] == target {
+			n = i
+		}
+	}
+
+	for i := ln - 1; i > 0; i-- {
+		if nums[i] == target {
+			n = i
+		}
+		if i > 0 && nums[i] < nums[i-1] {
+			break
+		}
+	}
+	return n
 }
