fc60098983d45f1597d462f1dfb4dcbd2d439ae8
itcuihao <it_cuihao@163.com>
2019-04-04 23:52:02 +0800

    lru

--- problems/0146.lru-cache/run.go
+++ problems/0146.lru-cache/run.go
@@ -21,6 +21,78 @@ func Run() {
 	fmt.Println(i)
 }
 
+// //  别人家的：
+// type node struct {
+// 	key  int
+// 	val  int
+// 	prev *node
+// 	next *node
+// }
+
+// type LRUCache struct {
+// 	capacity int
+// 	head     *node
+// 	mMap     map[int]*node
+// }
+
+// func Constructor(capacity int) LRUCache {
+// 	cache := LRUCache{capacity: capacity}
+// 	cache.mMap = make(map[int]*node)
+// 	cache.head = &node{key: -1, val: -1}
+
+// 	cur := cache.head
+// 	for i := 0; i < capacity-1; i++ {
+// 		node := &node{key: -1, val: -1}
+// 		cur.next = node
+// 		node.prev = cur
+// 		cur = node
+// 	}
+
+// 	cur.next = cache.head
+// 	cache.head.prev = cur
+
+// 	return cache
+// }
+
+// func (cache *LRUCache) move2Head(cur *node) {
+// 	if cur == cache.head {
+// 		cache.head = cache.head.prev
+// 		return
+// 	}
+
+// 	cur.prev.next = cur.next
+// 	cur.next.prev = cur.prev
+
+// 	cur.next = cache.head.next
+// 	cur.next.prev = cur
+// 	cache.head.next = cur
+// 	cur.prev = cache.head
+// }
+
+// func (cache *LRUCache) Get(key int) int {
+// 	node, ok := cache.mMap[key]
+// 	if !ok {
+// 		return -1
+// 	}
+// 	cache.move2Head(node)
+// 	return node.val
+// }
+
+// func (cache *LRUCache) Put(key int, value int) {
+// 	node, ok := cache.mMap[key]
+// 	if ok {
+// 		node.val = value
+// 		cache.move2Head(node)
+// 	} else {
+// 		oldKey := cache.head.key
+// 		cache.head.key = key
+// 		cache.head.val = value
+// 		delete(cache.mMap, oldKey)
+// 		cache.mMap[key] = cache.head
+// 		cache.head = cache.head.prev
+// 	}
+// }
+
 type LRUCache struct {
 	lcap  int
 	param map[int]int
@@ -41,7 +113,7 @@ func (this *LRUCache) Get(key int) int {
 		return -1
 	}
 	this.ext(key)
-	fmt.Printf("c:%v, p:%+v, r:%+v\n", this.lcap, this.param, this.cir)
+
 	return v
 }
 
@@ -54,33 +126,37 @@ func (this *LRUCache) Put(key int, value int) {
 	}
 
 	if this.lcap == len(this.cir) {
-		old := this.cir[0]
+		old := this.cir[len(this.cir)-1]
 		delete(this.param, old)
-		this.cir = this.cir[1:]
+		this.cir = this.cir[:len(this.cir)-1]
 	}
 	this.param[key] = value
 	this.cir = append(this.cir, key)
 	this.ext(key)
 
-	fmt.Printf("c:%v, p:%+v, r:%+v\n", this.lcap, this.param, this.cir)
-
 }
 
 func (this *LRUCache) ext(key int) {
+
+	list := make([]int, 0, len(this.cir))
 	index := 0
-	end := len(this.cir) - 1
+
 	for i, v := range this.cir {
 		if key == v {
 			index = i
 		}
 	}
-	this.cir[0], this.cir[index] = this.cir[index], this.cir[0]
-	this.cir[end], this.cir[index] = this.cir[index], this.cir[end]
+
+	list = append(list, this.cir[index])
+
+	this.cutc(index)
+
+	list = append(list, this.cir...)
+
+	this.cir = list
+
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * obj := Constructor(capacity);
- * param_1 := obj.Get(key);
- * obj.Put(key,value);
- */
+func (this *LRUCache) cutc(index int) {
+	this.cir = append(this.cir[:index], this.cir[index+1:]...)
+}
