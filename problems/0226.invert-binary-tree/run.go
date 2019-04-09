98f55571c0238ded5bc8a7331f91b899b22549ae
itcuihao <it_cuihao@163.com>
2019-04-09 23:40:10 +0800

    update

---
+++ problems/0226.invert-binary-tree/run.go
@@ -0,0 +1,17 @@
+
+package tree
+
+import(
+	"leetcode-go/structure"
+)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func invertTree(root *structure.TreeNode) *TreeNode {
+    
+}
---
+++ problems/0226.invert-binary-tree/run_test.go
@@ -0,0 +1,8 @@
+
+package tree
+
+import "testing"
+
+func TestRun(t *testing.T) {
+	Run()
+}
