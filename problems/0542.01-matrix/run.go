5eae8a2849defc368404c137e5a0372f88409676
itcuihao <it_cuihao@163.com>
2020-04-16 08:49:33 +0800

    add 542

--- problems/0542.01-matrix/run.go
+++ problems/0542.01-matrix/run.go
@@ -1,8 +1,13 @@
 package matrix
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
+// TODO: 先找个合适的队列
 func updateMatrix(matrix [][]int) [][]int {
+	list.New()
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[i]); j++ {
 			fmt.Println(matrix[i][j])
