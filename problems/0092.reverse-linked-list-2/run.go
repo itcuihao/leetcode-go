df5c89e4bfd9d80f466c1a1a11e052c1ea34d398
itcuihao <it_cuihao@163.com>
2020-09-06 22:43:46 +0800

    update

--- problems/0092.reverse-linked-list-2/run.go
+++ problems/0092.reverse-linked-list-2/run.go
@@ -14,6 +14,23 @@ func Run() {
  * }
  */
 func reverseBetween(head *structure.ListNode, m int, n int) *structure.ListNode {
-
-	return nil
+	if head == nil || m >= n {
+		return head
+	}
+	newHead := &structure.ListNode{Val: 0, Next: head}
+	pre := newHead
+	for count := 0; pre.Next != nil && count < m-1; count++ {
+		pre = pre.Next
+	}
+	if pre.Next == nil {
+		return head
+	}
+	cur := pre.Next
+	for i := 0; i < n-m; i++ {
+		tmp := pre.Next
+		pre.Next = cur.Next
+		cur.Next = cur.Next.Next
+		pre.Next.Next = tmp
+	}
+	return newHead.Next
 }
