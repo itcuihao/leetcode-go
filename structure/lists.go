397e04d269fc788faec62a89b0555bd075e24748
itcuihao <it_cuihao@163.com>
2020-09-07 08:17:47 +0800

    update:list print

--- structure/lists.go
+++ structure/lists.go
@@ -1,9 +1,20 @@
 package structure
 
+import "fmt"
+
 // ListNode 链表
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func (l *ListNode) Print() {
+	fmt.Println("start")
+	for l != nil {
+		fmt.Printf("%v -> ", l.Val)
+		l = l.Next
+	}
+	fmt.Println("\nend")
 }
 
 // Lists 链表
