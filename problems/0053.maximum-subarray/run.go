e3e76cb8cc8ca0c4d33b71c1bd01dc517972bb53
itcuihao <it_cuihao@163.com>
2020-09-08 23:34:05 +0800

    update:53 dp

--- problems/0053.maximum-subarray/run.go
+++ problems/0053.maximum-subarray/run.go
@@ -31,3 +31,25 @@ func maxSubArray(s []int) int {
 	}
 	return maxSoFar
 }
+
+func maxSubArrayDp(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp, res := make([]int, len(nums)), nums[0]
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = nums[i] + dp[i-1]
+		} else {
+			dp[i] = nums[i]
+		}
+		if res < dp[i] {
+			res = dp[i]
+		}
+	}
+	return res
+}
--- problems/0053.maximum-subarray/run_test.go
+++ problems/0053.maximum-subarray/run_test.go
@@ -6,3 +6,8 @@ func TestRun(t *testing.T) {
 	s := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	t.Log(maxSubArray(s))
 }
+
+func TestRun2(t *testing.T) {
+	s := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	t.Log(maxSubArrayDp(s))
+}
