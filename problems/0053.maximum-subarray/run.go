c2bc86f0203d1bd4100fe95610fb4996ef6a6f49
itcuihao <it_cuihao@163.com>
2020-09-08 08:46:55 +0800

    update:53

--- problems/0053.maximum-subarray/run.go
+++ problems/0053.maximum-subarray/run.go
@@ -1,5 +1,8 @@
 package subarray
 
+import "fmt"
+
+// 思路遍历整个数组，连续相加，索引前相加小于索引位置，则从索引位置开始相加
 func maxSubArray(s []int) int {
 	l := len(s)
 	if l == 0 {
@@ -15,13 +18,16 @@ func maxSubArray(s []int) int {
 	for i := 1; i < l; i++ {
 		if maxEndingHere+s[i] > s[i] {
 			maxEndingHere += s[i]
+			fmt.Println("a:", maxEndingHere)
 		} else {
 			maxEndingHere = s[i]
+			fmt.Println("b:", maxEndingHere)
 		}
 
 		if maxSoFar < maxEndingHere {
 			maxSoFar = maxEndingHere
 		}
+		fmt.Println("c:", maxSoFar)
 	}
 	return maxSoFar
 }
