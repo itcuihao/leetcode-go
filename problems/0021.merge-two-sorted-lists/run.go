607c6ad4d90be8df68fb4c2d4a0e48b765b64111
itcuihao <it_cuihao@163.com>
2020-09-11 08:43:37 +0800

    update: 21

--- problems/0021.merge-two-sorted-lists/run.go
+++ problems/0021.merge-two-sorted-lists/run.go
@@ -14,7 +14,6 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l2 == nil {
 		return l1
 	}
-
 	var head, node *ListNode
 
 	if l1.Val > l2.Val {
@@ -24,9 +23,7 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		head, node = l1, l1
 		l1 = l1.Next
 	}
-
 	for l1 != nil && l2 != nil {
-
 		if l1.Val < l2.Val {
 			node.Next = l1
 			l1 = l1.Next
@@ -34,18 +31,14 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 			node.Next = l2
 			l2 = l2.Next
 		}
-
 		node = node.Next
 	}
-
 	if l1 != nil {
 		node.Next = l1
 	}
-
 	if l2 != nil {
 		node.Next = l2
 	}
-
 	return head
 }
 
@@ -55,4 +48,20 @@ func printList(l *ListNode) {
 		fmt.Println("Val:", l.Val)
 		l = l.Next
 	}
+}
+
+// 递归的方式
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists2(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists2(l1, l2.Next)
+	return l2
 }
