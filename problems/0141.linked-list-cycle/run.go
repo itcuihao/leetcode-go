46c3ab265773151345929dfd7235839359472a3b
itcuihao <it_cuihao@163.com>
2020-12-23 23:50:39 +0800

    update:141

--- problems/0141.linked-list-cycle/run.go
+++ problems/0141.linked-list-cycle/run.go
@@ -15,16 +15,14 @@ func hasCycle(head *structure.ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
 	}
-
 	slow := head
 	fast := head
 	for fast != nil && fast.Next != nil {
-		if slow == fast {
-			// if slow.Val == fast.Val { // 模拟测试
-			return true
-		}
 		slow = slow.Next
 		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
 	}
 	return false
 }
