2e14d5d340f635b56808b0276a3fd4d7eff4d7c0
itcuihao <it_cuihao@163.com>
2021-01-31 00:30:30 +0800

    update

--- problems/0213.house-robber-ii/run.go
+++ problems/0213.house-robber-ii/run.go
@@ -1,10 +1,36 @@
 package ii
 
-func Run() {
+import "fmt"
 
+func Run() {
+	nums := []int{1, 2, 3, 1}
+	sum := rob(nums)
+	fmt.Println(sum)
 }
 
 func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	return max(myRob(nums[0:len(nums)-1]), myRob(nums[1:]))
+}
 
-	return 0
+func myRob(nums []int) int {
+	pre, cur, tmp := 0, 0, 0
+	for _, num := range nums {
+		tmp = cur
+		cur = max(pre+num, cur)
+		pre = tmp
+	}
+	return cur
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
