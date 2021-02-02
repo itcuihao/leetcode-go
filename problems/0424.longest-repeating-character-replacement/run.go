b3c685f5ff1ba3b777cf625b8716ff6ddd500079
itcuihao <it_cuihao@163.com>
2021-02-02 23:46:13 +0800

    update 424

--- problems/0424.longest-repeating-character-replacement/run.go
+++ problems/0424.longest-repeating-character-replacement/run.go
@@ -3,12 +3,36 @@ package replacement
 import "fmt"
 
 func Run() {
-	s := ""
+	s := "AABABBA"
 	k := 1
 	c := characterReplacement(s, k)
 	fmt.Println(c)
 }
 
 func characterReplacement(s string, k int) int {
+	num := make(map[uint8]int, 26)
+	ls := len(s)
+	maxn := 0
+	left := 0
+	right := 0
+	for right < ls {
+		indexRight := s[right] - 'A'
+		num[indexRight]++
+		maxn = max(maxn, num[indexRight])
 
+		if right-left+1-maxn > k {
+			indexLeft := s[left] - 'A'
+			num[indexLeft]--
+			left++
+		}
+		right++
+	}
+	return right - left
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
