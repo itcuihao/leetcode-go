d220687e5aebf13a60a4bfdd14c0591be4a8c7cc
itcuihao <it_cuihao@163.com>
2021-02-02 23:17:31 +0800

    add 424

---
+++ problems/0424.longest-repeating-character-replacement/run.go
@@ -0,0 +1,14 @@
+package replacement
+
+import "fmt"
+
+func Run() {
+	s := ""
+	k := 1
+	c := characterReplacement(s, k)
+	fmt.Println(c)
+}
+
+func characterReplacement(s string, k int) int {
+
+}
